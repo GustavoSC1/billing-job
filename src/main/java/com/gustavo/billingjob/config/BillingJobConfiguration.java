681b4b42ca02b2382971180808660f40a36735b3
GustavoSC1 <gu.cruz17@hotmail.com>
2024-10-25 17:24:47 -0300

    Initializing the project

---
+++ src/main/java/com/gustavo/billingjob/config/BillingJobConfiguration.java
@@ -0,0 +1,8 @@
+package com.gustavo.billingjob.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BillingJobConfiguration {
+
+}
---
+++ src/test/java/com/gustavo/billingjob/BillingJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.billingjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
