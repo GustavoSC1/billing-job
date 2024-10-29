f1469b5f65f774e7088b615f92c38798f5718bb9
GustavoSC1 <gu.cruz17@hotmail.com>
2024-10-29 17:26:22 -0300

    Using Job parameters to create Job Instances

--- src/main/java/com/gustavo/billingjob/config/BillingJob.java
+++ src/main/java/com/gustavo/billingjob/config/BillingJob.java
@@ -4,6 +4,7 @@ import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.repository.JobRepository;
 
 public class BillingJob implements Job {
@@ -22,11 +23,12 @@ public class BillingJob implements Job {
     @Override
     public void execute(JobExecution execution) {
     	try {
-	        System.out.println("processing billing information");
-	        
+    		JobParameters jobParameters = execution.getJobParameters();
+    		// Extraindo o parâmetro input.file do JobParameters
+	        String inputFile = jobParameters.getString("input.file");
+	        System.out.println("processing billing information from file " + inputFile); 
 	        // É responsabilidade da implementação do Job relatar seu status ao JobRepository.
-	        execution.setStatus(BatchStatus.COMPLETED);
-	
+	        execution.setStatus(BatchStatus.COMPLETED);	
 	        execution.setExitStatus(ExitStatus.COMPLETED);
     	} catch (Exception exception) {
             execution.addFailureException(exception);
--- src/test/java/com/gustavo/billingjob/BillingJobApplicationTests.java
+++ src/test/java/com/gustavo/billingjob/BillingJobApplicationTests.java
@@ -8,6 +8,7 @@ import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -28,14 +29,21 @@ class BillingJobApplicationTests {
 	@Test
 	void testJobExecution(CapturedOutput output) throws Exception {
 		// given
-		JobParameters jobParameters = new JobParameters();
+		// O JobParametersBuilder é a principal API fornecida pelo Spring Batch para construir um conjunto de JobParameters.
+		// Usamos esse construtor para criar um parâmetro do tipo String chamado input.file com o valor /some/input/file.
+		// Usamos também para criar um parâmetro do tipo String chamado file.format com o valor csv, além disso informamos 
+		// que o parâmetro file.format não é identificador.
+		JobParameters jobParameters = new JobParametersBuilder()
+				.addString("input.file", "/some/input/file")
+				.addString("file.format", "csv", false)
+				.toJobParameters();
 
 		// when
 		// Inicia o Job em lote com um conjunto de parâmetros.
 		JobExecution jobExecution = this.jobLauncher.run(this.job, jobParameters);
 
 		// then
-		Assertions.assertTrue(output.getOut().contains("processing billing information"));
+		Assertions.assertTrue(output.getOut().contains("processing billing information from file /some/input/file"));
 
 		Assertions.assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());	
 	}
